123ef5070775761bceb87de1418d4ef914f19322
Rana-hanii <ranahani797@gmail.com>
2025-04-25 22:04:13 +0300

    chat-ai ui

--- src/app/pages/chat-ai/chat-ai.component.ts
+++ src/app/pages/chat-ai/chat-ai.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-chat-ai',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './chat-ai.component.html',
   styleUrl: './chat-ai.component.css'
 })
