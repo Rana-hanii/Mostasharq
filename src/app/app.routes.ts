aa264da64129426b9a0b375fb2071b72a3129c78
Rana-hanii <ranahani797@gmail.com>
2025-04-25 12:03:18 +0300

    folder-structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+      },
+      {
+        path: 'chat-ai',
+        loadComponent: () => import('./pages/chat-ai/chat-ai.component').then(m => m.ChatAiComponent)
+      },
+      {
+        path: 'payment',
+        loadComponent: () => import('./pages/payment/payment.component').then(m => m.PaymentComponent)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/pages/login/login.component').then(m => m.LoginComponent)
+      },
+      {
+        path: 'sign-up',
+        loadComponent: () => import('./auth/pages/sign-up/sign-up.component').then(m => m.SignUpComponent)
+      },
+      {
+        path: 'forget',
+        loadComponent: () => import('./auth/pages/forget/forget.component').then(m => m.ForgetComponent)
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+
